761be2778e9e3cb06c2e5c4411ee0f455cff73d1
sanojsilva <silvasanoj@gmail.com>
2021-06-05 11:11:27 +0530

    Add fonts

---
+++ pages/post.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Post() {
+  return (
+    <div>
+      <h1 className="text-5xl font-playfair">Hello world</h1>
+      <h1 className="text-5xl font-montserrat">Hello world</h1>
+      <h1 className="text-5xl font-dosis">Hello world</h1>
+    </div>
+  );
+}
+
+export default Post;
