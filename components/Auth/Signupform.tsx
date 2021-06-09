63b7d4e2273a676a1c1171ef786395c70ab9a7aa
sanojsilva <silvasanoj@gmail.com>
2021-06-09 22:44:53 +0530

    Add signup functionality

--- components/Auth/Signupform.tsx
+++ components/Auth/Signupform.tsx
@@ -1,65 +1,84 @@
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 function Signupform() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleSubmit = async () => {
+    const result = await axios.post("http://localhost:4200/api/user/signup", {
+      name: username,
+      email: email,
+      password: pass,
+    });
+    console.log(result.data);
+  };
+
   return (
     <div>
       <div className="w-full max-w-xl ">
-        <h1 className="flex justify-center text-2xl p-5">Sign Up</h1>
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="flex justify-center p-5 text-2xl">Sign Up</h1>
+        <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
               Username
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
               placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
               Email Address
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               id="email"
               type="text"
               placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2">
+            <label className="block mb-2 text-sm font-bold text-gray-700">
               Password
             </label>
             <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
               placeholder="******************"
+              onChange={(e) => setPass(e.target.value)}
             />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
+            <p className="text-xs italic text-red-500">
+              Please choose a password. e
             </p>
           </div>
           <div className="flex items-center justify-between">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
               type="button"
+              onClick={handleSubmit}
             >
               Sign Up
             </button>
             <Link href="/login">
-            <a
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              href="#"
-            >
-              Already a member?
-            </a></Link>
+              <a
+                className="inline-block text-sm font-bold text-blue-500 align-baseline hover:text-blue-800"
+                href="#"
+              >
+                Already a member?
+              </a>
+            </Link>
           </div>
         </form>
-        <p className="text-center text-gray-500 text-xs">
+        <p className="text-xs text-center text-gray-500">
           &copy;2021 All rights reserved.
         </p>
       </div>
